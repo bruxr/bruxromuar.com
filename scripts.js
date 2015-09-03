14ae90b628978a3b783fe92055f3773e79e6527e
brux <brux.romuar@gmail.com>
2015-09-03 20:52:54 +0800

    add hero text ticker

--- scripts.js
+++ scripts.js
@@ -3,5 +3,6 @@
         if (Modernizr.mq('only screen and (min-width: 1200px)')) {
             $('#hero').height($(window).height() - 90);
         }
+        $('#rotate').airport(['amazing', 'interactive', 'memorable', 'helpful', 'cool']);
     });
 })(jQuery);
