8c8543713ec4a0baf21ef2473afd637a1cbdbc99
brux <brux.romuar@gmail.com>
2015-09-09 16:10:46 +0800

    remove viewport resizing scripts for now

--- scripts.js
+++
@@ -1,8 +0,0 @@
-(function($) {
-    $(document).on('ready', function() {
-        if (Modernizr.mq('only screen and (min-width: 1200px)')) {
-            $('#hero').height($(window).height() - 90);
-        }
-        $('#rotate').airport(['amazing', 'interactive', 'memorable', 'helpful', 'cool']);
-    });
-})(jQuery);
