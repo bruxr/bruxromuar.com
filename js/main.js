86a253c9c76384fb8161e05ecaadf7c90a0d6384
brux <brux.romuar@gmail.com>
2015-09-14 16:05:28 +0800

    touch accessibility

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 $(function() {
-
+    if (Modernizr.touch) {
+        var t;
+        $('.project').on('touchstart', function(e) {
+            var el = $(this);
+            t = setTimeout(function() {
+                $(this).toggleClass('hover');
+            }, 2000);
+        }).on('touchend', function() {
+            clearTimeout(t);
+        });
+    }
 });
