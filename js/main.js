fee2d7cfb7f912c7827c0f87e715a57091c74ef6
bruxromuar <brux.romuar@gmail.com>
2015-11-12 17:11:18 +0800

    remove touch interaction on mobile for now

--- js/main.js
+++ js/main.js
@@ -5,17 +5,4 @@ $(function() {
              scrollTop: $('#projects').position().top 
          }, 'normal', 'easeInQuart');
     });
-    
-    if (Modernizr.touch) {
-        var t;
-        $('.project').on('touchstart', function(e) {
-            e.preventDefault();
-            var el = $(this);
-            t = setTimeout(function() {
-                $(this).toggleClass('hover');
-            }, 2000);
-        }).on('touchend', function() {
-            clearTimeout(t);
-        });
-    }
 });
