b838a64df7b988605f8332054d857135bb66a8db
Brux Romuar <brux.romuar@gmail.com>
2022-02-07 15:45:39 +0800

    Add iter font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+
+import '@fontsource/inter/400.css';
+import '@fontsource/inter/variable.css';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
