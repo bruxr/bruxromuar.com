2102ec64d2b7641be7f12deef881bb6e1d7dadb0
Brux Romuar <brux.romuar@gmail.com>
2022-02-07 21:14:30 +0800

    Add initial homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,28 @@
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      black: '#000',
+      white: '#fff',
+      blue: {
+        200: '#e9f5fe',
+        800: '#466d8a',
+      },
+      gray: {
+        200: '#a0a0a0',
+        400: '#464646',
+      },
+    },
     fontFamily: {
       sans: ['Inter', 'sans-serif'],
     },
-    extend: {},
+    extend: {
+      spacing: {
+        68: '17rem',
+        88: '22rem',
+        112: '40rem',
+      },
+    },
   },
   plugins: [],
 };
